b9b627b6ee147a3e65809f12f2c8b7b41dec7330
Mike Winum <lazynorm@ml-winum.dk>
2025-04-16 00:06:00 +0200

    feature: model loads correctly (from home pc, not share-friendly yet) and new properties added to mesh class

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,15 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include "game.hpp"
+#include "mesh.hpp"
 
 int main(int argc, char *argv[])
 {
     Game game;
+
+    Mesh mesh;
+    mesh.LoadMesh("D:\\cpp\\fluid go swish\\test_elements\\cube.gltf"); // Load your model here
+
     game.Run();
     return 0;
 }
--- src/mesh.hpp
+++ src/mesh.hpp
@@ -1,6 +1,8 @@
 #ifndef __MESH_HPP__
 #define __MESH_HPP__
 
+#include <vector>
+
 class Mesh
 {
 public:
@@ -10,8 +12,13 @@ public:
     void LoadMesh(const char *path); // Load mesh from file
     void Draw();                     // Draw the mesh
 
-private:                // Private Methods
-private:                // Private members
+private:                                 // Private Methods
+private:                                 // Private members
+    std::vector<float> m_vertices;       // Vertex data
+    std::vector<float> m_normals;        // Normal data
+    std::vector<float> m_texCoords;      // Texture coordinate data
+    std::vector<unsigned int> m_indices; // Index data
+
     unsigned int m_VAO; // Vertex Array Object ID
     unsigned int m_VBO; // Vertex Buffer Object ID
     unsigned int m_EBO; // Element Buffer Object ID
