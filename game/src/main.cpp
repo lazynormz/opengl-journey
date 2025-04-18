2570738c6e40addfb4bd24b33b4fd0fa7c2c6b38
Mike Winum <lazynorm@ml-winum.dk>
2025-04-18 03:12:18 +0200

    feat: implements very basic gameobject

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,8 +1,31 @@
+#include <memory>
 #include <engine.hpp>
+
+class Box : public Actor::GameObject
+{
+public:
+    Box() : Actor::GameObject("") {};
+
+    Box(const char *modelPath) : Actor::GameObject(modelPath)
+    {
+    }
+
+    void Update() override
+    {
+        // Update logic for the box
+    }
+
+    void Render() override
+    {
+        m_mesh.Draw();
+    }
+};
 
 int main(int argc, char *argv[])
 {
     Core::Engine engine; // Create an instance of the Engine class
-    engine.Run();          // Run the game loop
-    return 0;              // Return success
+    Box b("D:/cpp/fluid go swish/test_elements/cube.gltf");
+    engine.RegisterGameObject(std::make_shared<Box>(b)); // Register the box game object
+    engine.Run();                                        // Run the game loop
+    return 0;                                            // Return success
 }
